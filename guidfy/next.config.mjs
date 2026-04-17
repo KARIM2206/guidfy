c51529159e50e5ec675b397f189ab0de174c6d79
Karim yousef <karim56780y@gmial.com>
2026-04-17 13:53:02 +0200

    chore: update config files

--- guidfy/next.config.mjs
+++ guidfy/next.config.mjs
@@ -1,17 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-    ],
+  //   domains: ['localhost','images.unsplash.com'],
+  //  // Add your backend host here
+  remotePatterns :[
+    {
+      hostname:'images.unsplash.com'
+    },
+    {
+      hostname:'localhost'
+    },
+    {
+      protocol: "https",
+      hostname:'via.placeholder.com'
+    }
+  ]
   },
-  reactCompiler: true,
+
 };
 
 export default nextConfig;
